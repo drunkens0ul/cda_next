128c51974474ad51d1d33af99839caddef415a5d
DrUnkeN-SoUL <mathewsshaji96@gmail.com>
2026-01-17 00:16:01 +0530

    REWORK

--- cda_app/components/icons/XIcon.tsx
+++
@@ -1,7 +0,0 @@
-export function XIcon({ className = '' }: { className?: string }) {
-  return (
-    <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24">
-      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 17L17 7M17 7H7M17 7V17" />
-    </svg>
-  )
-}
